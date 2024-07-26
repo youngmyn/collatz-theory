27e97c1dafabdecb30cb3825e84381e443d3b7c1
YoungMYN <Shyliko315on@gmail.com>
2024-07-26 23:10:57 +0300

    Optimizing lru cache by replacing maximumSize() with softValues(). Replacing BigDecimal with BigInteger, since the method only works with natural numbers. Adding a parity check to the isEven method to improve code readability

--- src/CollatzRun.java
+++ src/CollatzRun.java
@@ -1,18 +1,18 @@
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 
-import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class CollatzRun {
-    private static final long diapason = 999_999;
-    private static final Cache<BigDecimal, Long> collatzCache = CacheBuilder.newBuilder().maximumSize(Long.MAX_VALUE).build();
+    private static final long diapason = 50_000_000L;
+    private static final Cache<BigInteger, Long> collatzCache = CacheBuilder.newBuilder().softValues().build();
 
     public static void main(String[] args) {
         long maxSteps = 0;
         long maxHardNumber = 0;
         for (long i = 1; i < diapason; i++) {
-            long l = collatzFunc(BigDecimal.valueOf(i), 0);
-            collatzCache.put(BigDecimal.valueOf(i), l);
+            long l = collatzFunc(BigInteger.valueOf(i), 0);
+            collatzCache.put(BigInteger.valueOf(i), l);
 
             if (l > maxSteps) {
                 maxSteps = l;
@@ -21,21 +21,21 @@ public class CollatzRun {
             //TODO draw a graphic to show the steps.
         }
         System.out.println("The most hard humber is " + maxHardNumber + ". We need " + maxSteps + " steps to make 1");
-        //System.out.println("Cache contents:");
-        //collatzCache.asMap().forEach((key, value) -> System.out.println(key + " " + value));
     }
 
-    public static long collatzFunc(BigDecimal n, long stepsCounter) {
-        if (n.equals(BigDecimal.valueOf(1))) return stepsCounter;
-
-        if (n.remainder(BigDecimal.valueOf(2)).equals(BigDecimal.valueOf(0)))//if N is even, then divide by 2. (N = N/2)
-            n = n.divide(BigDecimal.valueOf(2));
-        else //if N is odd,N = N*3+1
-            n = n.multiply(BigDecimal.valueOf(3)).add(BigDecimal.valueOf(1));
+    public static long collatzFunc(BigInteger n, long stepsCounter) {
+        if (n.equals(BigInteger.ONE)) return stepsCounter;
+        if (isEven(n))
+            n = n.divide(BigInteger.TWO);
+        else
+            n = n.multiply(BigInteger.valueOf(3)).add(BigInteger.ONE);
 
         Long stepsInCache = collatzCache.getIfPresent(n);
         if (stepsInCache != null)//If the function has a value in the cache, we get it from there.
             return stepsInCache + stepsCounter + 1;
         else return collatzFunc(n, stepsCounter + 1);//otherwise, simple recursive call
+    }
+    public static boolean isEven(BigInteger number){
+        return !number.testBit(0);
     }
 }
